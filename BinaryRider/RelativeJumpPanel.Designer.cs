9760326d76318485e04554125ba72af50387eded
古橋 宏 <bryful@gmail.com>
2023-03-28 23:48:42 +0900

    update

--- BinaryRider/RelativeJumpPanel.Designer.cs
+++ BinaryRider/RelativeJumpPanel.Designer.cs
@@ -34,6 +34,9 @@
 			btnAddJump = new Button();
 			contextMenuStrip1 = new ContextMenuStrip(components);
 			button1 = new Button();
+			listBox1 = new ListBox();
+			button2 = new Button();
+			button3 = new Button();
 			SuspendLayout();
 			// 
 			// hexEdit1
@@ -53,6 +56,7 @@
 			// 
 			// btnSubJump
 			// 
+			btnSubJump.FlatStyle = FlatStyle.Flat;
 			btnSubJump.Location = new Point(140, 23);
 			btnSubJump.Name = "btnSubJump";
 			btnSubJump.Size = new Size(32, 32);
@@ -62,6 +66,7 @@
 			// 
 			// btnAddJump
 			// 
+			btnAddJump.FlatStyle = FlatStyle.Flat;
 			btnAddJump.Location = new Point(178, 23);
 			btnAddJump.Name = "btnAddJump";
 			btnAddJump.Size = new Size(32, 32);
@@ -76,6 +81,7 @@
 			// 
 			// button1
 			// 
+			button1.FlatStyle = FlatStyle.Flat;
 			button1.Location = new Point(110, 28);
 			button1.Name = "button1";
 			button1.Size = new Size(24, 22);
@@ -83,11 +89,46 @@
 			button1.Text = ">";
 			button1.UseVisualStyleBackColor = true;
 			// 
+			// listBox1
+			// 
+			listBox1.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+			listBox1.FormattingEnabled = true;
+			listBox1.ItemHeight = 21;
+			listBox1.Location = new Point(70, 70);
+			listBox1.Name = "listBox1";
+			listBox1.ScrollAlwaysVisible = true;
+			listBox1.Size = new Size(140, 130);
+			listBox1.Sorted = true;
+			listBox1.TabIndex = 5;
+			// 
+			// button2
+			// 
+			button2.FlatStyle = FlatStyle.Flat;
+			button2.Location = new Point(12, 70);
+			button2.Name = "button2";
+			button2.Size = new Size(52, 23);
+			button2.TabIndex = 6;
+			button2.Text = "Push";
+			button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			button3.FlatStyle = FlatStyle.Flat;
+			button3.Location = new Point(12, 175);
+			button3.Name = "button3";
+			button3.Size = new Size(52, 23);
+			button3.TabIndex = 7;
+			button3.Text = "Delete";
+			button3.UseVisualStyleBackColor = true;
+			// 
 			// RelativeJumpPanel
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(223, 58);
+			ClientSize = new Size(223, 210);
+			Controls.Add(button3);
+			Controls.Add(button2);
+			Controls.Add(listBox1);
 			Controls.Add(button1);
 			Controls.Add(btnAddJump);
 			Controls.Add(btnSubJump);
@@ -105,5 +146,8 @@
 		private Button btnAddJump;
 		private ContextMenuStrip contextMenuStrip1;
 		private Button button1;
+		private ListBox listBox1;
+		private Button button2;
+		private Button button3;
 	}
 }
